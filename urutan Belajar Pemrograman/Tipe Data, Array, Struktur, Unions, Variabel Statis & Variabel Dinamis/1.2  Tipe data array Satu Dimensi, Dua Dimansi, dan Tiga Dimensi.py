e63a3c7335f914147d81cc1087349abe6afda353
hijriah24 <hijriahijria74@gmail.com>
2025-12-08 21:40:38 +0800

    Create 1.2  Tipe data array Satu Dimensi, Dua Dimansi, dan Tiga Dimensi.py

---
+++ urutan Belajar Pemrograman/Tipe Data, Array, Struktur, Unions, Variabel Statis & Variabel Dinamis/1.2  Tipe data array Satu Dimensi, Dua Dimansi, dan Tiga Dimensi.py
@@ -0,0 +1,28 @@
+# hijriah sisfo a
+# File: array_dimensi.py
+# Deskripsi: Contoh array satu dimensi, dua dimensi, dan tiga dimensi menggunakan list Python.
+
+# Array 1 dimensi
+array1D = [10, 20, 30, 40]
+print("Array 1D:", array1D)
+
+# Array 2 dimensi
+array2D = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print("Array 2D:", array2D)
+
+# Array 3 dimensi
+array3D = [
+    [
+        [1, 2], 
+        [3, 4]
+    ],
+    [
+        [5, 6], 
+        [7, 8]
+    ]
+]
+print("Array 3D:", array3D)
